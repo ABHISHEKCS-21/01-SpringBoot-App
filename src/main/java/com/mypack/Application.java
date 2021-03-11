8e8e0efae1c26a9f92061032df3888ce60e94706
Abhishek <sanjeetk4676@gmail.com>
2021-03-11 13:43:29 +0530

    Main method bug fixed

--- src/main/java/com/mypack/Application.java
+++ src/main/java/com/mypack/Application.java
@@ -5,10 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-		//kkkkkkkkkkkkkkk
+		
+		//Some modification done ok
 	}
 
 }
