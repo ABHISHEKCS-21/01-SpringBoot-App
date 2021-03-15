93d70790cc43573a79753caeae4b53bcab4e7014
Abhishek <sanjeetk4676@gmail.com>
2021-03-15 11:05:10 +0530

    feature-11 branch add

--- src/main/java/com/mypack/Application.java
+++ src/main/java/com/mypack/Application.java
@@ -6,11 +6,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
 	
-
+	git -b feature-11 repo URL;
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 		
 		//Some modification done ok
 	}
+
+	public String App()
+		{
+		//Adding meathod by feature-11 branch;
+		}
 
 }
